ffae4e0aabe6ff3653a5d71d2daadf4447d3948b
sandorfalot <sandorfalot@gmail.com>
2021-09-24 01:12:22 -0400

    Comitted 112am

--- mystupidapp/settings.py
+++ mystupidapp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 # Make a secret key
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-plrk0^dms*yj*cy9nlj0$@%&9m8#rxl_*4l8%!77#xbl!77-nm')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-plrk0^dms*yj*cy9nlj0$@%&9m8#rxl_*4l8%!77#xbl!77-nm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

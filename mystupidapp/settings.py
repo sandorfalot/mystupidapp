0eed4e51e9307cb3504c670f20d485e3c87dd15e
sandorfalot <sandorfalot@gmail.com>
2021-09-24 01:24:44 -0400

    Changedfiles 124am

--- mystupidapp/settings.py
+++ mystupidapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import dj_database_url
+import django_heroku
 import os
 from pathlib import Path
 # Make a secret key
@@ -137,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
